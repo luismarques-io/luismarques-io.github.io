aee7b01a708b4255c2988a6ce9a02d408b6c9e91
Luís Marques <euluismarques@gmail.com>
2021-07-27 22:02:34 +0000

    design experience section

--- js/main.js
+++ js/main.js
@@ -1,64 +1,63 @@
-$(function () {
+$(function () {});
 
+// Tabs - Based on https://codepen.io/mburridge/pen/rNVeaKv
+tabs({
+	buttonSelector: ".jobs__tabs__button",
+	contentSelector: ".job__content",
 });
 
-// JS Tabs
-// 1: https://stackoverflow.com/questions/59432791/how-to-make-tabs-with-pure-js
-// 2: https://codepen.io/wangel13/pen/OXBrRp
+function tabs(opt) {
+	// DOM Elements
+	const tabs = document.querySelectorAll(opt.buttonSelector);
+	const tabContents = document.querySelectorAll(opt.contentSelector);
 
-function Tabs() {
-    var bindAll = function () {
-        var menuElements = document.querySelectorAll('[data-tab]');
-        for (var i = 0; i < menuElements.length; i++) {
-            menuElements[i].addEventListener('click', change, false);
-        }
-    }
+	// Functions
+	const activateTab = (tabnum) => {
+		tabs.forEach((tab) => {
+			tab.classList.remove("active");
+		});
 
-    var clear = function () {
-        var menuElements = document.querySelectorAll('[data-tab]');
-        for (var i = 0; i < menuElements.length; i++) {
-            menuElements[i].classList.remove('active');
-            var id = menuElements[i].getAttribute('data-tab');
-            document.getElementById(id).classList.remove('active');
-        }
-    }
+		tabContents.forEach((tabContent) => {
+			tabContent.classList.remove("active");
+		});
+		document.querySelector("#tab__" + tabnum).classList.add("active");
+		document
+			.querySelector("#tabcontent__" + tabnum)
+			.classList.add("active");
+	};
 
-    var change = function (e) {
-        clear();
-        e.target.classList.add('active');
-        var id = e.currentTarget.getAttribute('data-tab');
-        document.getElementById(id).classList.add('active');
-    }
+	// Event Listeners
+	tabs.forEach((tab) => {
+		tab.addEventListener("click", () => {
+			activateTab(tab.dataset.tab);
+		});
+	});
 
-    bindAll();
+	// Select first as default
+	tabs[0].classList.add("active");
+	tabContents[0].classList.add("active");
 }
 
-var connectTabs = new Tabs();
-
-
-
-
-
-// JS Scroll Reveal - https://codepen.io/kccnma/pen/MQeXPY
+// JS Scroll Reveal - Based on https://codepen.io/kccnma/pen/MQeXPY
 
 // reveal point from bottom and top of the window
-var srPoint = 250;
-window.addEventListener('scroll', reveal);
+var srPoint = 150;
+window.addEventListener("scroll", reveal);
 reveal();
 
 function reveal() {
-    var srSelector = document.querySelectorAll('.scrollreveal');
-    for (var i = 0; i < srSelector.length; i++) {
-        var windowHeight = window.innerHeight;
-        var srTop = srSelector[i].getBoundingClientRect().top;
-        var srBottom = srSelector[i].getBoundingClientRect().bottom;
-        if (srTop < windowHeight - srPoint) {
-            srSelector[i].classList.add('scrollreveal__active')
-        } else {
-            // srSelector[i].classList.remove('scrollreveal__active');
-        };
-        if (srBottom < 0 + srPoint) {
-            // srSelector[i].classList.remove('scrollreveal__active');
-        }
-    }
-};
+	var srSelector = document.querySelectorAll(".scrollreveal");
+	for (var i = 0; i < srSelector.length; i++) {
+		var windowHeight = window.innerHeight;
+		var srTop = srSelector[i].getBoundingClientRect().top;
+		var srBottom = srSelector[i].getBoundingClientRect().bottom;
+		if (srTop < windowHeight - srPoint) {
+			srSelector[i].classList.add("scrollreveal__active");
+		} else {
+			// srSelector[i].classList.remove('scrollreveal__active');
+		}
+		if (srBottom < 0 + srPoint) {
+			// srSelector[i].classList.remove('scrollreveal__active');
+		}
+	}
+}
