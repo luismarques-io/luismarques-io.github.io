80c5e6fa26ab96ff5b131661ed02c548f85d4449
Luís Marques <euluismarques@gmail.com>
2021-08-10 23:28:12 +0000

    add copy e-mail to clipboard functionality to intro section

--- js/main.js
+++ js/main.js
@@ -1,4 +1,23 @@
 $(function () {
+	// Birds -
+	VANTA.BIRDS({
+		el: ".background__birds",
+		mouseControls: true,
+		touchControls: true,
+		gyroControls: false,
+		minHeight: 200.0,
+		minWidth: 200.0,
+		scale: 1.0,
+		scaleMobile: 1.0,
+		backgroundColor: 0xffffff,
+		color1: 0x542abb,
+		colorMode: "lerp",
+		birdSize: 2, // 1.5,
+		separation: 19.0,
+		cohesion: 23.0,
+		quantity: 1.0,
+		backgroundAlpha: 0.0,
+	});
 	// VANTA.HALO({
 	// 	el: ".intro",
 	// 	mouseControls: true,
@@ -8,7 +27,6 @@ $(function () {
 	// 	minWidth: 200.0,
 	// 	amplitudeFactor: 1.2,
 	// });
-
 	// Halo - https://www.vantajs.com/?effect=halo#(amplitudeFactor:1,backgroundAlpha:1,backgroundColor:332112,baseColor:1334188,color2:15918901,gyroControls:!f,minHeight:200,minWidth:200,mouseControls:!t,mouseEase:!t,ringFactor:1,rotationFactor:1,scale:1,scaleMobile:1,size:3,speed:1,touchControls:!t,xOffset:0.16,yOffset:0.32)
 	// VANTA.HALO({
 	// 	el: ".intro",
@@ -23,7 +41,6 @@ $(function () {
 	// 	yOffset: 0.32,
 	// 	size: 3.0,
 	// });
-
 	// // Fog - https://www.vantajs.com/?effect=fog#(backgroundAlpha:1,baseColor:15527676,blurFactor:0.9,gyroControls:!f,highlightColor:9216749,lowlightColor:8123903,midtoneColor:16741936,minHeight:200,minWidth:200,mouseControls:!t,scale:2,scaleMobile:4,speed:2.4000000000000004,touchControls:!t,zoom:0.5)
 	// VANTA.FOG({
 	// 	el: ".intro",
@@ -40,59 +57,55 @@ $(function () {
 	// 	speed: 2.4,
 	// 	zoom: 0.5,
 	// });
-
-	// Birds -
-	VANTA.BIRDS({
-		el: ".background__birds",
-		mouseControls: true,
-		touchControls: true,
-		gyroControls: false,
-		minHeight: 200.0,
-		minWidth: 200.0,
-		scale: 1.0,
-		scaleMobile: 1.0,
-		backgroundColor: 0xffffff,
-		color1: 0x542abb,
-		colorMode: "lerp",
-		birdSize: 1.5,
-		separation: 19.0,
-		cohesion: 23.0,
-		quantity: 1.0,
-		backgroundAlpha: 0.0,
-	});
-
 	// Waves - https://www.vantajs.com/?effect=waves#(backgroundAlpha:1,color:11316396,gyroControls:!f,minHeight:200,minWidth:200,mouseControls:!t,scale:1,scaleMobile:1,shininess:150,touchControls:!t,waveHeight:12.5,waveSpeed:0.25,zoom:0.65)
-	VANTA.WAVES({
-		el: ".skills",
-		mouseControls: true,
-		touchControls: true,
-		gyroControls: false,
-		minHeight: 200.0,
-		minWidth: 200.0,
-		scale: 1.0,
-		scaleMobile: 1.0,
-		color: 0xacacac,
-		shininess: 150.0,
-		waveHeight: 12.5,
-		waveSpeed: 0.25,
-		zoom: 0.65,
-	});
-
+	// VANTA.WAVES({
+	// 	el: ".skills",
+	// 	mouseControls: true,
+	// 	touchControls: true,
+	// 	gyroControls: false,
+	// 	minHeight: 200.0,
+	// 	minWidth: 200.0,
+	// 	scale: 1.0,
+	// 	scaleMobile: 1.0,
+	// 	color: 0xacacac,
+	// 	shininess: 150.0,
+	// 	waveHeight: 12.5,
+	// 	waveSpeed: 0.25,
+	// 	zoom: 0.65,
+	// });
 	// Globe
-	VANTA.GLOBE({
-		el: ".pub_awards",
-		mouseControls: true,
-		touchControls: true,
-		gyroControls: false,
-		minHeight: 200.0,
-		minWidth: 200.0,
-		scale: 1.0,
-		scaleMobile: 1.0,
-		color: 0x3fffbb,
-		color2: 0xbebebe,
-		backgroundColor: 0xffffff,
+	// VANTA.GLOBE({
+	// 	el: ".pub_awards",
+	// 	mouseControls: true,
+	// 	touchControls: true,
+	// 	gyroControls: false,
+	// 	minHeight: 200.0,
+	// 	minWidth: 200.0,
+	// 	scale: 1.0,
+	// 	scaleMobile: 1.0,
+	// 	color: 0x3fffbb,
+	// 	color2: 0xbebebe,
+	// 	backgroundColor: 0xffffff,
+	// });
+
+	$("[data-copy-to-clipboard]").click(function (e) {
+		e.preventDefault();
+		const $btn = $(this);
+		copyToClipboard($btn.data("copy-to-clipboard"));
+		$btn.addClass("copied");
+		setTimeout(() => {
+			$btn.removeClass("copied");
+		}, 1000);
 	});
 });
+
+function copyToClipboard(text) {
+	var $temp = $("<input>");
+	$("body").append($temp);
+	$temp.val(text).select();
+	document.execCommand("copy");
+	$temp.remove();
+}
 
 // Tabs - Based on https://codepen.io/mburridge/pen/rNVeaKv
 tabs({
