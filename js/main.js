e4054d8cbd16e28c73ac802e861eb33e47e6b7d9
Luís Marques <luismarques@Air-de-Luis.lan>
2021-08-12 00:23:16 +0100

    redesign skills section

--- js/main.js
+++ js/main.js
@@ -35,29 +35,6 @@ $(function () {
 		backgroundAlpha: 0.0,
 	});
 
-	// VANTA.HALO({
-	// 	el: ".intro",
-	// 	mouseControls: true,
-	// 	touchControls: true,
-	// 	gyroControls: false,
-	// 	minHeight: 200.0,
-	// 	minWidth: 200.0,
-	// 	amplitudeFactor: 1.2,
-	// });
-	// Halo - https://www.vantajs.com/?effect=halo#(amplitudeFactor:1,backgroundAlpha:1,backgroundColor:332112,baseColor:1334188,color2:15918901,gyroControls:!f,minHeight:200,minWidth:200,mouseControls:!t,mouseEase:!t,ringFactor:1,rotationFactor:1,scale:1,scaleMobile:1,size:3,speed:1,touchControls:!t,xOffset:0.16,yOffset:0.32)
-	// VANTA.HALO({
-	// 	el: ".intro",
-	// 	mouseControls: true,
-	// 	touchControls: true,
-	// 	gyroControls: false,
-	// 	minHeight: 200.0,
-	// 	minWidth: 200.0,
-	// 	baseColor: 0x145bac,
-	// 	backgroundColor: 0x51150,
-	// 	xOffset: 0.16,
-	// 	yOffset: 0.32,
-	// 	size: 3.0,
-	// });
 	// Fog - https://www.vantajs.com/?effect=fog#(backgroundAlpha:1,baseColor:15527676,blurFactor:0.9,gyroControls:!f,highlightColor:9216749,lowlightColor:8123903,midtoneColor:16741936,minHeight:200,minWidth:200,mouseControls:!t,scale:2,scaleMobile:4,speed:2.4000000000000004,touchControls:!t,zoom:0.5)
 	VANTA.FOG({
 		el: ".background__fog",
@@ -105,6 +82,31 @@ $(function () {
 		speed: 5,
 		zoom: 0.5,
 	});
+
+	// VANTA.HALO({
+	// 	el: ".intro",
+	// 	mouseControls: true,
+	// 	touchControls: true,
+	// 	gyroControls: false,
+	// 	minHeight: 200.0,
+	// 	minWidth: 200.0,
+	// 	amplitudeFactor: 1.2,
+	// });
+	// Halo - https://www.vantajs.com/?effect=halo#(amplitudeFactor:1,backgroundAlpha:1,backgroundColor:332112,baseColor:1334188,color2:15918901,gyroControls:!f,minHeight:200,minWidth:200,mouseControls:!t,mouseEase:!t,ringFactor:1,rotationFactor:1,scale:1,scaleMobile:1,size:3,speed:1,touchControls:!t,xOffset:0.16,yOffset:0.32)
+	// VANTA.HALO({
+	// 	el: ".intro",
+	// 	mouseControls: true,
+	// 	touchControls: true,
+	// 	gyroControls: false,
+	// 	minHeight: 200.0,
+	// 	minWidth: 200.0,
+	// 	baseColor: 0x145bac,
+	// 	backgroundColor: 0x51150,
+	// 	xOffset: 0.16,
+	// 	yOffset: 0.32,
+	// 	size: 3.0,
+	// });
+
 	// Waves - https://www.vantajs.com/?effect=waves#(backgroundAlpha:1,color:11316396,gyroControls:!f,minHeight:200,minWidth:200,mouseControls:!t,scale:1,scaleMobile:1,shininess:150,touchControls:!t,waveHeight:12.5,waveSpeed:0.25,zoom:0.65)
 	// VANTA.WAVES({
 	// 	el: ".skills",
