f79a6fbb9efcc30cdd5359efefff0235cec59f16
Luís Marques <luismarques@Air-de-Luis.lan>
2021-08-13 02:13:03 +0100

    general design details

--- js/scripts/hobbies.js
+++ js/scripts/hobbies.js
@@ -1,4 +1,10 @@
 $(function () {
+	$(".hobbies__blobs .blob").each(function () {
+		$(this).css("border-radius", generateBorderRadiusBlob());
+	});
+});
+
+$(function () {
 	$(".hobbies__item__image img").each(function () {
 		$(this).css("border-radius", generateBorderRadiusBlob());
 	});
