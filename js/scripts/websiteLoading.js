bae8feecf64b16a3cdfb675b9ff8a1738d412bd5
Luís Marques <euluismarques@gmail.com>
2021-08-23 16:41:39 +0000

    longer loading with more opacity

--- js/scripts/websiteLoading.js
+++ js/scripts/websiteLoading.js
@@ -2,14 +2,14 @@ $(document).ready(function () {
 	const $wrap = $(".website__loading__enable");
 	setTimeout(() => {
 		$wrap.addClass("website__loading1");
-	}, 100);
+	}, 1);
 	setTimeout(() => {
 		$wrap.addClass("website__loading2");
-	}, 1000);
+	}, 1200);
 	setTimeout(() => {
 		$wrap.addClass("website__loading__complete");
 	}, 2000);
 	setTimeout(() => {
 		$wrap.addClass("website__loading__complete2");
-	}, 2250);
+	}, 2500);
 });
